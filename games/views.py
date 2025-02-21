505cd3ee3568591b1421a84a23dfaa2c6eac75b2
asilbek <asilbekmirolimov@gmail.com>
2025-02-21 09:14:20 +0500

    changes

--- games/views.py
+++ games/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Game
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 def home_page(request):
@@ -28,3 +31,41 @@ def remote_page(request):
 
 def video_page(request):
     return render(request, 'video.html')
+
+def login_page(request):  # username, password
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(username=username, password=password)
+        print(user)
+        
+        if user is not None:
+            login(request, user)
+            print('User logged in')
+            return render(request, 'index.html')
+        else:
+            print('User not found')
+            return render(request, 'login.html')
+    return render(request, 'login.html')
+
+
+
+def register_page(request):  # full_name, username, email, telefon, password, confirm_password
+    if request.method == 'POST':
+        full_name = request.POST.get('full_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        telefon = request.POST.get('telefon')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        
+        if password == confirm_password:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.first_name = full_name
+            user.save()
+            return render(request, 'login.html')
+        else:
+            return render(request, 'register.html')
+    return render(request, 'register.html')
+
--- website/urls.py
+++ website/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from games.views import home_page, about_page, contact_page, remote_page, video_page, products_page, contact_page
+from games.views import home_page, about_page, contact_page, remote_page, video_page, products_page, contact_page\
+    , login_page, register_page
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -30,6 +31,10 @@ urlpatterns = [
     path('contact/', contact_page, name='contact'),
     path('remote/', remote_page, name='remote'),
     path('video/', video_page, name='video'),
+
+    path('login/', login_page, name='login'),
+    path('register/', register_page, name='register'),
+    
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
